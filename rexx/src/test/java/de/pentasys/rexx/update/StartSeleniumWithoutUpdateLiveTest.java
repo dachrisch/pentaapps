2c794294f36a8d4207624c9ef284ed2d83dccb7f
Christian Dähn <christian.daehn@pentasys.de>
2011-05-30 14:04:39 +0200

    line feed problems

--- rexx/src/test/java/de/pentasys/rexx/update/StartSeleniumWithoutUpdateLiveTest.java
+++ rexx/src/test/java/de/pentasys/rexx/update/StartSeleniumWithoutUpdateLiveTest.java
@@ -4,19 +4,17 @@ import static de.pentasys.selenium.setup.SeleniumSetup.createSeleniumInstance;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriverBackedSelenium;
 
 public class StartSeleniumWithoutUpdateLiveTest {
-    
 
-    @Test
-    public void startSeleniumInOrderToCheckClasspath() throws Exception {
-        final WebDriverBackedSelenium selenium = createSeleniumInstance("http://google.com");
+	@Test
+	public void startSeleniumInOrderToCheckClasspath() throws Exception {
+		final WebDriverBackedSelenium selenium = createSeleniumInstance("http://google.com");
 
-        assertThat(selenium, notNullValue());
+		assertThat(selenium, notNullValue());
 
-        selenium.stop();
-    }
+		selenium.stop();
+	}
 }
