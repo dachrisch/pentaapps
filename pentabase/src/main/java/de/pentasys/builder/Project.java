18aa259effbffdb663cc1a354ce0e6d677f49ec6
Christian Dähn <christian.daehn@pentasys.de>
2011-05-31 18:54:32 +0200

    added intern project

--- pentabase/src/main/java/de/pentasys/builder/Project.java
+++ pentabase/src/main/java/de/pentasys/builder/Project.java
@@ -1,17 +1,17 @@
 package de.pentasys.builder;
 
 public enum Project {
-    MEDIASATURN("P080811.MED"), LASTUNDPERFORMANCETESTS("P100802.WWK");
+	MEDIASATURN("P080811.MED"), LASTUNDPERFORMANCETESTS("P100802.WWK"), INTERN("PMMUC01.INT");
 
-    private final String projectId;
+	private final String projectId;
 
-    Project(final String projectId) {
-        this.projectId = projectId;
+	Project(final String projectId) {
+		this.projectId = projectId;
 
-    }
+	}
 
-    public String getProjectId() {
-        return projectId;
-    }
+	public String getProjectId() {
+		return projectId;
+	}
 
 }
